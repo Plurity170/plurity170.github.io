82bc0860aefe17b32b0f02da45991746d8166639
Plurity170 <devinedongo28@gmail.com>
2026-01-07 09:30:53 +0200

    Update script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("actionBtn").addEventListener("click", () => {
-  document.getElementById("status").textContent =
-    "Thanks for visiting. More content will be added soon.";
-});
